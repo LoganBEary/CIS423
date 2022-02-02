4f028467adb1ea2616d2bf9aee44a2fa9621a924
LoganLevitre <45443105+LoganBEary@users.noreply.github.com>
2022-02-02 11:00:27 -0800

    Update imports.py

--- imports.py
+++ imports.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sklearn import metrics
 import matplotlib.pyplot as plt
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
